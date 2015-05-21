f21b34ff91dee5f5ebc92d673826780e168ae3d5
accentconcierge <vyasa@accent.global>
2015-05-21 10:53:41 -0400

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :businesses
   resources :service_histories
   devise_for :users
-  resources :users, :path => 'u'
-
+  #resources :users, :path => 'u'
+ 
 
   root 'pages#index'
   get 'new' => 'pages#new'
